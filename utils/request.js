8f4dc363a6acc77216c09f7ab791f139a5f75413
xiaoyang.li <xiaoyang.li@nio.com>
2018-07-25 17:40:46 +0800

    request.js

---
+++ utils/request.js
@@ -0,0 +1,37 @@
+const httpRequest = data => {
+  let code = "";
+  wx.getStorage({
+    key: "code",
+    success: res => {
+      code = res.code;
+      return new Promise(function(resolve, reject) {
+        //发起网络请求
+        wx.request({
+          url: data.url,
+          data: data.data,
+          method: data.method,
+          header: {
+            code: code
+          },
+          success: function(res) {
+            if (res.data.success) {
+              resolve(res.data);
+            } else {
+              console.log(JSON.stringify(res));
+              reject(res.data);
+            }
+          },
+          fail: function(res) {
+            console.log(JSON.stringify(res));
+            reject(res);
+          }
+        });
+      });
+    },
+    fail: res => {
+      console.log("not found code in storage");
+    }
+  });
+};
+
+export default httpRequest;
