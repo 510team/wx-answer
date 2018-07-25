8dc4f6739bc0a32a2497db20d2863c15cc105637
fan zhang <fan.zhang1@nio.com>
2018-07-25 17:10:08 +0800

    添加 app

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,7 @@ Page({
     })
   },
   onLoad: function () {
+    console.log(app.globalData.userInfo);
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -25,6 +26,7 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log(res);
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
