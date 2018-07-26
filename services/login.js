32d1e672395ef5ce953fba678bf2d240728ed21e
xiaoyang.li <xiaoyang.li@nio.com>
2018-07-26 17:03:53 +0800

    login

--- services/login.js
+++ services/login.js
@@ -22,7 +22,6 @@ const getUserSettingApi = () => {
     wx.getSetting({
       success: res => {
         if (res.authSetting["scope.userInfo"]) {
-          debugger;
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           resolve(res);
         }
