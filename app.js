1697c0e536d2d9a86010e8ea9ef32c5f73fb3234
xiaoyang.li <xiaoyang.li@nio.com>
2018-08-10 18:22:17 +0800

    答题结束接口

--- app.js
+++ app.js
@@ -77,17 +77,23 @@ App({
       fail: res => {
         console.log("get code faild");
         loginApi()
-          .then(code => {
-            this.globalData.isLogin = true;
-            loginRequest(code);
-          })
-          .then(data => {
-            //code 成功
-            console.log("login request result", data);
-            wx.setStorage({
-              key: "code",
-              data: data
-            });
+          .then(code => loginRequest(code))
+          .then(res => {
+            const data = res.data.data;
+            console.log("login", data);
+            if (data && data.userInfo) {
+              this.globalData.userInfo = data.userInfo;
+              //由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(data);
+              }
+            }
+            if (data && data.code) {
+              wx.setStorage({
+                key: "code",
+                data: data.code
+              });
+            }
           })
           .catch(data => {
             console.error("data", data);
