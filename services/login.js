5a0364fe93cf93a0f38d8c1c6b0f6fff3dd9e0fe
xiaoyang.li <xiaoyang.li@nio.com>
2018-08-15 17:13:07 +0800

    排行榜页面 样式调整

--- services/login.js
+++ services/login.js
@@ -24,12 +24,7 @@ const getUserSettingApi = () => {
       success: res => {
         if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          console.log(
-            "已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框"
-          );
           resolve(res);
-        } else {
-          console.log("未授权，弹窗");
         }
       }
     });
