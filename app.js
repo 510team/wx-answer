2eb77d499bbc7f49152a74a3bab35e0e67cabb7e
xiaoyang.li <xiaoyang.li@nio.com>
2018-08-15 17:13:45 +0800

    排行榜页面 样式调整

--- app.js
+++ app.js
@@ -49,17 +49,17 @@ App({
       // 如果已经登录过，则跳过登录
       success: res => {
         console.log("get code success");
-        // findUserRequest().then(data => {
-        //   if (data && data.userInfo && data.userInfo.avatarUrl) {
-        //     console.log("findUserRequest request result", data);
-        //     this.globalData.userInfo = data.userInfo;
-        //     //由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 所以此处加入 callback 以防止这种情况
-        //     if (this.userInfoReadyCallback) {
-        //       this.userInfoReadyCallback(data);
-        //     }
-        //     return data;
-        //   }
-        // });
+        findUserRequest().then(data => {
+          if (data && data.userInfo && data.userInfo.avatarUrl) {
+            console.log("findUserRequest request result", data);
+            this.globalData.userInfo = data.userInfo;
+            //由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回 所以此处加入 callback 以防止这种情况
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(data);
+            }
+            return data;
+          }
+        });
 
         getUserSettingApi()
           .then(() => getUserInfoApi())
