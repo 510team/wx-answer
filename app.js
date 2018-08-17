19b9851568f00b64f9db50649d6f648d5c657411
fan zhang <fan.zhang1@nio.com>
2018-08-17 16:58:57 +0800

    提交更新

--- app.js
+++ app.js
@@ -3,30 +3,8 @@ import { loginAction, getUserInfoAction } from "./services/login.js";
 
 App({
   onLaunch: function() {
-    //检查用户登录状态
-    // wx.getStorage({
-    //   key: "code",
-    //   success: res => {
-    //     wx.checkSession({
-    //       success: res => {
-    //         console.log("登陆状态未失效");
-    //       },
-    //       fail: err => {
-    //         // 重新登录
-    //         console.log("登陆状态失效");
-    //         loginAction();
-    //       }
-    //     });
-    //   },
-    //   fail: res => {
-    //     loginAction();
-    //   }
-    // });
-    // getUserInfoAction();
   },
 
   globalData: {
-    userInfo: null,
-    isLogin: false
   }
 });
