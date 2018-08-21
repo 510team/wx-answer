a67607b8fed894fb28cd2cbb6015cafd6958b8eb
zilong.hao <zilong.hao@nio.com>
2018-08-21 19:36:28 +0800

    add 用户详情

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,116 +6,118 @@ import { getAllBackground } from "../../services/background";
 import { serverHost } from "../../config/index";
 
 import {
-  loginApi,
-  getUserInfoApi,
-  getUserSettingApi,
-  loginRequest,
-  setUserRequest
+    loginApi,
+    getUserInfoApi,
+    getUserSettingApi,
+    loginRequest,
+    setUserRequest
 } from "../../services/login.js";
 import { testRequest, updateScoreRequest } from "../../services/test.js";
 //获取应用实例
 const app = getApp();
 
 Page({
-  data: {
-    motto: "Hello World",
-    userInfo: {},
-    hasUserInfo: false,
-    level: "",
-    canIUse: wx.canIUse("button.open-type.getUserInfo"),
-    backgroundUrl: ""
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: "../logs/logs"
-    });
-  },
-  onLoad: function() {
-    wx.showShareMenu({
-      withShareTicket: true
-    });
-    //检查code
-    wx.getStorage({
-      key: "code",
-      success: res => {
-        getUserLevel().then(res => {
-          this.setData({
-            level: res.current_level.name
-          });
+    data: {
+        motto: "Hello World",
+        userInfo: {},
+        hasUserInfo: false,
+        level: "",
+        canIUse: wx.canIUse("button.open-type.getUserInfo"),
+        backgroundUrl: ""
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: "../logs/logs"
         });
-        getAllBackground().then(res => {
-          if (res.success) {
-            app.globalData.background = serverHost + res.data.cur;
-            this.setData({ backgroundUrl: app.globalData.background });
-          }
+    },
+    onLoad: function() {
+        wx.showShareMenu({
+            withShareTicket: true
         });
-        wx.checkSession({
-          success: res => {
-            console.log("登陆状态未失效");
-          },
-          fail: err => {
-            // 重新登录
-            console.log("登陆状态失效");
-            loginAction();
-          }
+        //检查code
+        wx.getStorage({
+            key: "code",
+            success: res => {
+                getUserLevel().then(res => {
+                    this.setData({
+                        level: res.current_level.name
+                    });
+                });
+                getAllBackground().then(res => {
+                    if (res.success) {
+                        app.globalData.background = serverHost + res.data.cur;
+                        this.setData({
+                            backgroundUrl: app.globalData.background
+                        });
+                    }
+                });
+                wx.checkSession({
+                    success: res => {
+                        console.log("登陆状态未失效");
+                    },
+                    fail: err => {
+                        // 重新登录
+                        console.log("登陆状态失效");
+                        loginAction();
+                    }
+                });
+            },
+            fail: res => {
+                loginAction();
+            }
         });
-      },
-      fail: res => {
-        loginAction();
-      }
-    });
-    //检查userInfo
-    getUserInfoApi()
-      .then(res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        });
-        wx.setStorage({
-          key: "hasUserInfo",
-          data: res.userInfo
-        });
-        return res;
-      })
-      .then(res => setUserRequest(res.rawData, res.signature));
-  },
+        //检查userInfo
+        getUserInfoApi()
+            .then(res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                });
+                wx.setStorage({
+                    key: "hasUserInfo",
+                    data: res.userInfo
+                });
+                return res;
+            })
+            .then(res => setUserRequest(res.rawData, res.signature));
+    },
 
-  getUserInfo: function(e) {
-    if (e.detail.userInfo) {
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      });
-      wx.setStorage({
-        key: "hasUserInfo",
-        data: e.detail.userInfo
-      });
-      setUserRequest(e.detail.rawData, e.detail.signature);
+    getUserInfo: function(e) {
+        if (e.detail.userInfo) {
+            this.setData({
+                userInfo: e.detail.userInfo,
+                hasUserInfo: true
+            });
+            wx.setStorage({
+                key: "hasUserInfo",
+                data: e.detail.userInfo
+            });
+            setUserRequest(e.detail.rawData, e.detail.signature);
+        }
+    },
+    onNav(e) {
+        wx.navigateTo({
+            url: e.target.dataset.url
+        });
+        wx.reportAnalytics("link_to_page", {
+            url: e.target.dataset.url
+        });
+    },
+    onShareAppMessage(options) {
+        if (options.from === "button") {
+            console.log(options.target);
+        }
+        return {
+            title: "敢来和我pk下吗？",
+            path: "pages/index/index",
+            imageUrl: "../../assets/image/share.jpg",
+            success: function(res) {
+                console.log("res", res);
+            },
+            fail: function(res) {
+                console.log("转发到群失败");
+            }
+        };
     }
-  },
-  onNav(e) {
-    wx.navigateTo({
-      url: e.target.dataset.url
-    });
-    wx.reportAnalytics("link_to_page", {
-      url: e.target.dataset.url
-    });
-  },
-  onShareAppMessage(options) {
-    if (options.from === "button") {
-      console.log(options.target);
-    }
-    return {
-      title: "敢来和我pk下吗？",
-      path: "pages/index/index",
-      imageUrl: "../../assets/image/share.jpg",
-      success: function(res) {
-        console.log("res", res);
-      },
-      fail: function(res) {
-        console.log("转发到群失败");
-      }
-    };
-  }
 });
